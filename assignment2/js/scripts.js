fb98010cda6189f1d8bba751e006c030a932b173
algonick <algonick@my.yorku.ca>
2024-08-09 17:33:37 -0400

    Add files via upload

--- assignment2/js/scripts.js
+++ assignment2/js/scripts.js
@@ -136,7 +136,18 @@ document.addEventListener('DOMContentLoaded', function() {
   
     renderPost = (post) => {
         const postElement = document.createElement('div');
-        postElement.innerHTML = "<div class= 'card mb-4 '><a href= '#! '><img class= 'card-img-top ' src=${post.img} alt=${post.headline}/></a><div class= 'card-body '><div class= 'small text-muted '>${post.date}</div><h2 class= 'card-title '>${post.headline}</h2><p class= 'card-text '>${post.teaser}</p><a id=more-${post.id} class= 'btn btn-primary ' onclick=showBlog(${post.id})  href= '# '>Read more →</a><div id= '${post.id} ' class= 'article-body ' style= 'display: none; '><p id= 'article-start '></p>${post.body}</div></div></div>";   
+        postElement.innerHTML = `
+            <div class= 'card mb-4 '><a href= '#! '><img class= 'card-img-top ' src=${post.img} alt=${post.headline}/></a>
+                <div class= 'card-body '>
+                    <div class= 'small text-muted '>${post.date}</div>
+                    <h2 class= 'card-title '>${post.headline}</h2>
+                    <p class= 'card-text '>${post.teaser}</p>
+                    <a id=more-${post.id} class= 'btn btn-primary ' onclick=showBlog(${post.id})  href= '# '>Read more →</a>
+                        <div id= '${post.id} ' class= 'article-body ' style= 'display: none; '>
+                            <p id= 'article-start '></p>${post.body}
+                        </div>
+                </div>
+            </div>`;   
         
         if (postNum < 1) {
             spotlight.appendChild(postElement); 
