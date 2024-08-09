56e9e02068b3ee9e67d2b4a8a876efd0f20119cd
algonick <algonick@my.yorku.ca>
2024-08-09 16:32:06 -0400

    Add files via upload

--- assignment2/js/scripts.js
+++ assignment2/js/scripts.js
@@ -131,16 +131,17 @@ document.addEventListener('DOMContentLoaded', function() {
             // manipulate postElement to show the content of the blog post with the specific style defined for it
             if (postNum < 1) {
                 spotlight.appendChild(postElement); 
+                postNum++;
             } else {
                 moreBlogs.appendChild(postElement);
             }
             //blogList.appendChild(postElement);
-        });
-
-        renderPost = (post) => {
-            postElement.innerHTML = "<div class= 'card mb-4 '><a href= '#! '><img class= 'card-img-top ' src=${post.img} alt=${post.headline}/></a><div class= 'card-body '><div class= 'small text-muted '>${post.date}</div><h2 class= 'card-title '>${post.headline}</h2><p class= 'card-text '>${post.teaser}</p><a id=more-${post.id} class= 'btn btn-primary ' onclick=showBlog(${post.id})  href= '# '>Read more →</a><div id= '${post.id} ' class= 'article-body ' style= 'display: none; '><p id= 'article-start '></p>${post.body}</div></div></div>";    
-        }        
-
-    .catch(error => console.error('Error loading blog posts:', error));
-    });
+        })
+    
+    .catch(error => { console.error('Error loading blog posts:', error) });
+  
+    renderPost = (post) => {
+        postElement.innerHTML = "<div class= 'card mb-4 '><a href= '#! '><img class= 'card-img-top ' src=${post.img} alt=${post.headline}/></a><div class= 'card-body '><div class= 'small text-muted '>${post.date}</div><h2 class= 'card-title '>${post.headline}</h2><p class= 'card-text '>${post.teaser}</p><a id=more-${post.id} class= 'btn btn-primary ' onclick=showBlog(${post.id})  href= '# '>Read more →</a><div id= '${post.id} ' class= 'article-body ' style= 'display: none; '><p id= 'article-start '></p>${post.body}</div></div></div>";    
+    }        
+        
 });
